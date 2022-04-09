28387149a9c99ad3ae4d354328ebcdd1df471ba9
GemPegas <77661722+BlueGemPegasus@users.noreply.github.com>
2022-04-10 00:11:23 +0800

    Arena is now  place able.
    - Arena is now place able
    - Auto Hide Joysticks when it's not in use
    - Plane Detection will stop and Plane that formed will be deleted.
    
    What Left need to do?
    - Player Spawning
    - Aiming with Raycast
    - Networking
    - Lobby

--- ARShootingGame/Assets/Scripts/PlaceArena.cs
+++ ARShootingGame/Assets/Scripts/PlaceArena.cs
@@ -2,17 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UI; // For Debug Issue.
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 [RequireComponent(typeof(ARRaycastManager))]
 public class PlaceArena : MonoBehaviour
 {
-    /* The main inclusion component in order for AR to detect
-     * Do not delete/change (Unless you know what are you doing)
-     */
+    // Component //
     private ARRaycastManager _arRaycastManager;
     private Pose placementPose;
     private bool placementPoseIsValid = false;
@@ -20,45 +16,35 @@ public class PlaceArena : MonoBehaviour
     // This is the Placement Indicator //
     public GameObject GhostArena;
 
-    
-
+    // The Arena to replace GhostArena, and start the game.
     public GameObject Arena;
-    private GameObject TheSpawnedArena;
-   
-    private Vector2 touchposition;
 
-    private PlayerInput playerInput;
+    public GameObject _btnPlaceArena;
+    public GameObject JoySticks;
+
+    // If Arena is placed, this set to true, GhostArena will be hidden from sight, so is Plane Scanner
+    bool ArenaPlaced = false;
 
     private void Awake()
     {
         // Getting the Component
         _arRaycastManager = GetComponent<ARRaycastManager>();
-        playerInput = new PlayerInput();
+        _btnPlaceArena = GameObject.Find("Btn_PlaceArena");
+        JoySticks = GameObject.Find("JoySticks");
 
         // First Hide The Ghost Arena
         GhostArena.SetActive(false);
+        _btnPlaceArena.SetActive(false);
+        JoySticks.SetActive(false);
     }
 
     private void Update()
     {
-        UpdatePlacementIndicator();
-        UpdatePlacementPose();
-        
-        
-        //if (_arRaycastManager.Raycast(touchposition, hits, TrackableType.PlaneWithinPolygon))
-        //{
-        //    var hitPose = hits[0].pose;
-            
-        //    if (TheSpawnedArena == null)
-        //    {
-        //        TheSpawnedArena = Instantiate(Arena, hitPose.position, hitPose.rotation);
-        //    }
-        //    else
-        //    {
-        //        TheSpawnedArena.transform.position = hitPose.position;
-        //
-        //}
-            
+        if (!ArenaPlaced)
+        {
+            UpdatePlacementIndicator();
+            UpdatePlacementPose();
+        }
     }
 
     private void UpdatePlacementIndicator()
@@ -67,34 +53,23 @@ public class PlaceArena : MonoBehaviour
         {
             GhostArena.SetActive(true);
             GhostArena.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+            _btnPlaceArena.SetActive(true);
         }
         else
         {
             GhostArena.SetActive(false);
+            _btnPlaceArena.SetActive(false);
         }
     }
 
+    // This function is to Update the "Ghost Arena" position, inorder for player to place the real arena.
     private void UpdatePlacementPose()
     {
         var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
         var hits = new List<ARRaycastHit>();
         _arRaycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinPolygon);
 
-        placementPoseIsValid = hits.Count > 0;
-        /*
-         * Debug Area
-         * There is a problem that the "Ghost Arena" wouldn't move with the camera
-         *
-         *
-         * After Debug
-         * Hits.Count == 0, but the Ghost Arena did instantly set Active as soon as the game start
-         */
-        Text Debugger = GameObject.Find("Debugger").GetComponent<Text>();
-        if (Debugger != null)
-        {
-            Debugger.text = hits.Count.ToString();
-        }
-        
+        placementPoseIsValid = hits.Count > 0; 
         if (placementPoseIsValid)
         {
             placementPose = hits[0].pose;
@@ -103,5 +78,24 @@ public class PlaceArena : MonoBehaviour
             var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
             placementPose.rotation = Quaternion.LookRotation(cameraBearing);
         }
+    }
+
+    // Placing the Arena only allow the Host to place
+    // This require Networking.
+    // After the Arena placed, only spawn both player
+    // Or maybe, try to do : " Only the Arena placed, other player can join the lan-server etcs "
+    public void PlacingArena()
+    {
+        ArenaPlaced = true;
+        Instantiate(Arena, GhostArena.transform.position, GhostArena.transform.rotation);
+        GhostArena.SetActive(false);
+        ARPlaneManager _arPlaneManager = FindObjectOfType<ARPlaneManager>();
+        foreach (var plane in _arPlaneManager.trackables)
+        {
+            plane.gameObject.SetActive(false);
+        }
+        _arPlaneManager.enabled = !_arPlaneManager.enabled;
+        _btnPlaceArena.SetActive(false);
+        JoySticks.SetActive(true);
     }
 }
